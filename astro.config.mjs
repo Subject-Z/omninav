0581ac337ec57654a0c240a269322ab47744ddda
Subject-Z <seedlx@qq.com>
2025-03-25 10:32:10 +0800

    Merge remote changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://subject-z.github.io', // 替换为您的 GitHub Pages URL
-  base: '/omninav', // 如果部署到 username.github.io/omninav，否则如果是用户站点可以删除此行
-  outDir: './dist', // 明确指定输出目录（可选，因为这是默认值）
+  site: 'https://subject-z.github.io/omninav', 
+  base: '/omninav',
+  outDir: './dist',
   integrations: [
     sitemap({
       // i18n设置
