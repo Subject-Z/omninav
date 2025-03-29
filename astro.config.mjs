9a4af477881d08ab8ce2538948f27981a3b25f96
Subject-Z <seedlx@qq.com>
2025-03-30 03:09:00 +0800

    Initial commit from local project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://omninav.uk',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'zh'],
+    routing: {
+      prefixDefaultLocale: false,
+    }
+  },
+  integrations: [
+    sitemap({
+      // i18n 配置
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en-US',
+          zh: 'zh-CN'
+        }
+      }
+    })
+  ]
+});
